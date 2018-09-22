7099753c8aa09c285a453788c97fd3bc847750fa
Chamod <chamodwijesena77@gmail.com>
2018-09-22 15:50:23 +0530

    do the changes

--- resources/views/inc/navebar.blade.php
+++ resources/views/inc/navebar.blade.php
@@ -1,18 +1,55 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-      <a class="navbar-brand" href="/"><h4>Medireps</h4></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="/Patient">Patient</a>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="about.html">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="services.html">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.html">Contact</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Registations
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+            <a class="dropdown-item" href="/Patient">1 Pation Registation</a>
+            <a class="dropdown-item" href="portfolio-2-col.html">2 Column Portfolio</a>
+            <a class="dropdown-item" href="portfolio-3-col.html">3 Column Portfolio</a>
+            <a class="dropdown-item" href="portfolio-4-col.html">4 Column Portfolio</a>
+            <a class="dropdown-item" href="portfolio-item.html">Single Portfolio Item</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Blog
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+            <a class="dropdown-item" href="blog-home-1.html">Blog Home 1</a>
+            <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
+            <a class="dropdown-item" href="blog-post.html">Blog Post</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Other Pages
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+            <a class="dropdown-item" href="full-width.html">Full Width Page</a>
+            <a class="dropdown-item" href="sidebar.html">Sidebar Page</a>
+            <a class="dropdown-item" href="faq.html">FAQ</a>
+            <a class="dropdown-item" href="404.html">404</a>
+            <a class="dropdown-item" href="pricing.html">Pricing Table</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -2,45 +2,26 @@
 <html>
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/modern-business.css" rel="stylesheet">
   <title>Medireps</title>
-  <link rel="stylesheet" href="/css/app.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+    <!-- Navigation -->
   @include('inc.navebar')
-
-
-
-<div class="row col-md-13">
-  <div class="col-md-1">
-  </div>
-    <div class="col-md-11">
-<div  id='bodycontent' >
-  <div style="margin-top:10px">
-    @if(Request::is('/'))
-      @include('inc.showcase')
-    @endif
-  </div>
-
-  <div class="row">
-      <div class="col-md-8 ">
-        @include('inc.messages');
-        @yield('content')
-      </div>
-      <div class="col-md-4 ">
-        @include('inc.sidebar')
-      </div>
-  </div>
-</div>
-</div>
-</div>
+    <!-- Messages -->
+  @include('inc.messages');
+    <!-- Page Content -->
+  @yield('content')
 
   <footer id="footer" class="text-center" >
-  <p>copyrigth 2018 &copy; Acme</p>
-</footer>
-
+    <p>copyrigth 2018 &copy; Acme</p>
+  </footer>
 </body>
 </html>
